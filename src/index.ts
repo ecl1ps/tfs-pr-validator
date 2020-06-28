f0abd0385135f694002d4f609bd3db9611e275be
Eclips <eclips@x.y>
2020-06-28 21:47:42 +0200

    Added API key configuration through environment variable

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { runServer } from "./server.ts";
 import { controller } from "./controller.ts";
 import { StatusClient } from "./status/StatusClient.ts";
 
-const apiToken = "";
+const TFS_API_ENV_KEY = "TFS_API_KEY";
+
+const apiToken = Deno.env.get(TFS_API_ENV_KEY);
+if (!apiToken)
+    throw new Error(`TFS API access token must be set in environment variable ${TFS_API_ENV_KEY}. See https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate for instructions.`);
+
 const statusClient = new StatusClient(apiToken);
 runServer((request) => controller(request, statusClient));
